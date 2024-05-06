2a66b3b4bf36e2ed636ef832fc68d5c14c6111a1
Apple <apple@Apples-MacBook-Pro.local>
2024-05-06 15:31:16 -0500

    update deface overrides

--- app/overrides/add_admin_product_form_fields.rb
+++ app/overrides/add_admin_product_form_fields.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(virtual_path: 'spree/admin/products/_form',
+AddAdminProductFormFields = Deface::Override.new(virtual_path: 'spree/admin/products/_form',
                      name: 'product_assembly_admin_product_form_right',
                      insert_after: "[data-hook='admin_product_form_description']",
                      partial: 'spree/admin/products/product_assembly_fields',
--- app/overrides/add_admin_tabs.rb
+++ app/overrides/add_admin_tabs.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_product_tabs',
+AddAdminTabs = Deface::Override.new(virtual_path: 'spree/admin/shared/_product_tabs',
                      name: 'product_assembly_admin_product_tabs',
                      insert_bottom: "[data-hook='admin_product_tabs']",
                      insert_after: "erb[silent]:contains('content_for(:page_tabs)')",
