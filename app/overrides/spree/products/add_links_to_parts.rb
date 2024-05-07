0d13931aa7441970ab15e7b225114b973d6aafe5
Apple <apple@Apples-MacBook-Pro.local>
2024-05-07 14:23:27 -0500

    update defaces

--- app/overrides/spree/products/add_links_to_parts.rb
+++ app/overrides/spree/products/add_links_to_parts.rb
@@ -1,4 +1,4 @@
-AddLinksToParts = Deface::Override.new(
+Spree::Products::AddLinksToParts = Deface::Override.new(
   virtual_path: "spree/products/_cart_form",
   name: "add_links_to_parts",
   insert_bottom: "[data-hook='inside_product_cart_form']",
--- app/overrides/spree/products/show/remove_add_to_cart_button_for_non_individual_sale_products.rb
+++ app/overrides/spree/products/show/remove_add_to_cart_button_for_non_individual_sale_products.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(
+Spree::Products::Show::RemoveAddToCartButtonForNonIndividualSaleProducts = Deface::Override.new(
   virtual_path: 'spree/products/show',
   name:         'remove_add_to_cart_button_for_non_individual_sale_products',
   surround:     '[data-hook="cart_form"]',
