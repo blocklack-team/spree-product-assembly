b9e11046a4d731c1b65e022c3bd45b7ffc4e1d10
Apple <apple@Apples-MacBook-Pro.local>
2024-05-07 12:19:39 -0500

    update partial

--- app/overrides/add_admin_tabs.rb
+++ app/overrides/add_admin_tabs.rb
@@ -1,5 +1,8 @@
-AddAdminTabs = Deface::Override.new(virtual_path: 'spree/admin/shared/_product_tabs',
-                     name: 'product_assembly_admin_product_tabs',
-                     insert_bottom: "[data-nav-x-content]",
-                     partial: 'spree/admin/shared/product_assembly_product_tabs',
-                     disabled: false)
+AddAdminTabs = Deface::Override.new(
+    virtual_path: 'spree/products/show',
+    name: 'product_assembly_admin_product_tabs',
+    insert_after: "[data-hook='admin_product_tabs'] li[data-hook='admin_tab_digital_assets']",
+    partial: 'spree/admin/shared/product_assembly_product_tabs',
+    disabled: false
+)
+                      
