640df5efee039c4c819bb273d566d45620580c2d
Apple <apple@Apples-MacBook-Pro.local>
2024-05-07 14:42:40 -0500

    update bundler

--- app/overrides/spree/admin/orders/_shipment/stock_contents.rb
+++ app/overrides/spree/admin/orders/_shipment/stock_contents.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(
+Spree::Admin::Orders::Shipment::StockContents = Deface::Override.new(
   virtual_path: 'spree/admin/orders/_shipment',
   name: 'stock_contents',
   replace_contents: '.stock-contents tbody',
